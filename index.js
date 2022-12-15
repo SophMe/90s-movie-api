fd68bc1a397f71e0d190651b3af272c6c45f4d5d
SophMe <116891325+SophMe@users.noreply.github.com>
2022-12-15 13:43:04 +0100

    add users and movie data

--- index.js
+++ index.js
@@ -8,68 +8,200 @@ const app = express();
 
 app.use(bodyParser.json());
 
+let users = [
+	{
+		id: 1,
+		name: "Sophie",
+		favoriteMovies: []
+	},
+	{
+		id: 2,
+		name: "Teresa",
+		favoriteMovies: ["Braveheart"]
+	},
+	{
+		id: 3,
+		name: "Kasia",
+		favoriteMovies: []
+	},
+];
+
 let movies = [
 	{
 		title: "Amercian Beauty",
-		director: "Sam Mendes",
+		year: 1999,
+		director: {
+			name: "Sam Mendes", 
+			birth: 1965.0
+		},
 		main: "Kevin Spacey",
-		genre: "Drama"
+		genre: {
+			name: "Drama",
+			description: "Some text",
+		},
 	},
 	{
 		title: "Shawshenk Redemption",
-		director: "Frank Darabont",
+		year: 1994,
+		director: {
+			name: "Frank Darabont", 
+			birth: 1959.0
+		},
 		main: "Tim Robbins",
-		genre: "Drama"
+		genre: {
+			name: "Drama",
+			description: "Some text",
+		},
 	},
 	{
 		title: "Braveheart",
-		director: "Mel Gibson",
+		year: 1995,
+		director: {
+			name: "Mel Gibson",
+			birth: 1956.0
+		},
 		main: "Mel Gibson",
-		genre: "Historical drama"
+		genre: {
+			name: "Historical drama",
+			description: "Some text",
+		},
 	},
 	{
 		title: "Forrest Gump",
-		director: "Robert Zemeckis",
+		year: 1994,
+		director: {
+			name: "Robert Zemeckis",
+			birth: 1952.0
+		},
 		main: "Tom Hanks",
 		genre: "Comedy-drama"
 	},
 	{
 		title: "Fight Club",
-		director: "David Fincher",
+		year: 1999,
+		director: {
+			name: "David Fincher",
+			birth: 1962.0
+		},
 		main: "Brad Pitt",
 		genre: "Pycho-thriller"
 	},
 	{
 		title: "The English Patient",
-		director: "Anthony Minghella",
+		year: 1996,
+		director: {
+			name: "Anthony Minghella",
+			birth: 1954.0
+		},
 		main: "Ralph Fiennes",
 		genre: "Romantic drama"
 	},
 	{
 		title: "Trainspotting",
-		director: "Danny Boyle",
+		year: 1996,
+		director: {
+			name: "Danny Boyle",
+			birth: 1956.0
+		},
 		main: "Ewan McGregor",
 		genre: "Black comdey-drama"
 	},
 	{
 		title: "Groundhog Day",
-		director: "Joel Coen",
+		year: 1993,
+		director: {
+			name: "Joel Coen",
+			birth: 1944.0
+		},
 		main: "Bill Murray",
 		genre: "Comedy"
 	},
 	{
 		title: "The Piano",
-		director: "Jane Campion",
+		year: 1993,
+		director: {
+			name: "Jane Campion",
+			birth: 1954.0
+		},
 		main: "Holly Hunter",
 		genre: "Drama"
 	},
 	{
 		title: "Elizabeth",
-		director: "Shekhar Kapur",
+		year: 1998,
+		director: {
+			name: "Shekhar Kapur",
+			birth: 1945.0
+		},
 		main: "Cate Blanchet",
 		genre: "Historical drama"
 	},
 ];
+
+//CREATE new user
+app.post('/users', (req, res) => {
+	//bodyParser enables us to request from the body object
+	const newUser = req.body;
+	if (newUser.name) {
+		newUser.id = uuid.v4();
+		users.push(newUser);
+		res.status(201).json(newUser)
+	} else {
+		res.status(400).send('please enter a name')
+	}
+});
+
+//UPDATE user name
+app.put('/users/:id', (req, res) => {
+	const { id } = req.params;
+	const updatedUser = req.body;
+	let user = users.find( user => user.id == id ); // == because we are looking for a string, thruthy
+	if (user) {
+		user.name = updatedUser.name;
+		res.status(200).json(user);
+	} else {
+		res.status(400).send('no such user')
+	}
+});
+
+//DELETE user
+app.delete('/users/:id/', (req, res) => {
+	const { id } = req.params;
+	
+	let user = users.find( user => user.id == id );
+	if (user) {
+		users = users.filter( user => user.id != id);
+		res.status(200).send(' user ${id} has been deleted');
+	} else {
+		res.status(400).send('no such user')
+	}
+});
+
+//CREATE (add) movie to favorites
+app.post('/users/:id/:movieTitle', (req, res) => {
+	const { id, movieTitle } = req.params;
+	
+	let user = users.find( user => user.id == id );
+	if (user) {
+		user.favoriteMovies.push(movieTitle);
+		res.status(200).send('${movieName} has been added to ${id}\'s array');
+	} else {
+		res.status(400).send('no such user')
+	}
+});
+
+//DELETE movie from favorites
+app.delete('/users/:id/:movieTitle', (req, res) => {
+	const { id, movieTitle } = req.params;
+	
+	let user = users.find( user => user.id == id );
+	if (user) {
+		user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+		res.status(200).send('${movieTitle} has been removed from ${id}\'s array');
+	} else {
+		res.status(400).send('no such user')
+	}
+});
 
 //return JSON object containing movie data
 app.get('/movies', (req, res) => {
@@ -110,14 +242,23 @@ app.get('/movies/:title', (req, res) => {
 	}
 });
 
-//one genre by name (of genre)
+//genre by name (of genre)
 app.get('/movies/genre/:genreName', (req, res) => {
-	//const title = req.params.title;
-	//object destructuring
 	const { genreName } = req.params;
 	const genre = movies.find(movie => movie.genre.name === genreName).genre;
 	if (genre) {
 		res.status(200).json(genre);
+	} else {
+		res.status(400).send('no such genre');
+	}
+});
+
+//director by name
+app.get('/movies/directors/:directorName', (req, res) => {
+	const { directorName } = req.params;
+	const director = movies.find(movie => movie.director.name === directorName).director;
+	if (director) {
+		res.status(200).json(director);
 	} else {
 		res.status(400).send('no such genre');
 	}
