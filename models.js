dc1fcb448c05bac39a68b59fc656f50a79f93c67
SophMe <116891325+SophMe@users.noreply.github.com>
2023-01-12 11:47:48 +0100

    add bcrypt and hashedPassword

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 
 // define schema for collections (movies + users) in MongoDB
 let movieSchema = mongoose.Schema({
@@ -26,6 +26,15 @@ let userSchema = mongoose.Schema({
     Birthday: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}] // reference db.movies collection
 });
+
+// bcrypt
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) { //Don't use arrow functions when defining instance methods!
+    return bcrypt.compareSync(password, this.Password);
+};
 
 // create models that use the schemas
 let Movie = mongoose.model('Movie', movieSchema); // "Movie" creates the collection "db.movies"
