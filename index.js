06d2c1cd21084f8992d2b7471871d6f459d56a4e
SophMe <116891325+SophMe@users.noreply.github.com>
2023-01-12 11:47:15 +0100

    add CORS and hashedPassword

--- index.js
+++ index.js
@@ -24,9 +24,26 @@ mongoose.connect('mongodb://127.0.0.1:27017/movie-apiDB', {useNewUrlParser: true
 app.use(bodyParser.json()); //MIDDLEWARE will run every time we go to a specific route
 app.use(bodyParser.urlencoded({extended: true}));
 
+//cross-origin resource sharing
+//default allows requests from all origins
+const cors = require('cors');
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a origin is not on the list of allowed origins
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 let auth = require('./auth')(app); // app ensures that Express is available in auth.js as well
 const passport = require('passport');
 	require('./passport');
+
 
 //ROUTES with Express
 
@@ -101,15 +118,16 @@ app.get('/movies/Director/:Name', passport.authenticate('jwt', { session: false 
 //USERS
 //CREATE new user with Mongoose
 app.post('/users', (req, res) => {
-	Users.findOne({Username: req.body.Username})			// check if user already exists
+	let hashedPassword = Users.hashPassword(req.body.Password); // hashes password when registering
+	Users.findOne({Username: req.body.Username})								// check if user already exists
 		.then((user) => {
 			if (user) {
 				return res.status(400).send(req.body.Username + 'already exists');
 			} else {
 				Users
-					.create({																	// if not, create new user
-						Username: req.body.Username,						// every key correponds to field specified in schema at models.js
-						Password: req.body.Password,
+					.create({														// if not, create new user
+						Username: req.body.Username,			// every key correponds to field specified in schema at models.js
+						Password: hashedPassword,
 						Email: req.body.Email,
 						Birthday: req.body.Birthday
 					})
@@ -128,19 +146,19 @@ app.post('/users', (req, res) => {
 
 //CREATE (add) movie to favorites 
 app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
-	Users.findOneAndUpdate({Username: req.params.Username}, {
-	   $push: {FavoriteMovies: req.params.MovieID}
-	 },
-	 {new: true}, // return the updated document
+	Users.findOneAndUpdate({ Username: req.params.Username }, {
+		$push: { FavoriteMovies: req.params.MovieID }
+	},
+	{ new: true}, // return updated document
 	(err, updatedUser) => {
-	  if (err) {
-		console.error(err);
-		res.status(500).send('Error: ' + err);
-	  } else {
-		res.json(updatedUser);
-	  }
+		if(err) {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		} else {
+			res.json(updatedUser);
+		}
 	});
-  });
+});
 
 //READ
 //Get all users
@@ -177,7 +195,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (r
       Birthday: req.body.Birthday
     }
   },
-  {new: true}, // return the updated document
+  {new: true}, // return updated document
   (err, updatedUser) => {
     if(err) {
       console.error(err);
