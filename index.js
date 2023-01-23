d20d50231edef2df04f38c228ef310e43644cddc
SophMe <116891325+SophMe@users.noreply.github.com>
2023-01-23 16:24:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //default allows requests from all origins
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234/'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
