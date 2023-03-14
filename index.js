ff744c4dbc6ad7de9cdead58a292f23e00177db5
SophMe <116891325+SophMe@users.noreply.github.com>
2023-03-14 14:22:12 +0100

    update allowedOrigins CORS

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //default allows requests from all origins
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:1234', 'https://90s-movie-api-liart.vercel.app', 'https://90s-movie-api-sophme.vercel.app/', 'http://testsite.com', 'https://en.wikipedia.org', 'https://www.wikipedia.org/', 'https://90s-movies.netlify.app/'];
+let allowedOrigins = ['http://localhost:1234', 'https://90s-movies.netlify.app/', 'https://90s-movie-api-sophme.vercel.app/', 'http://testsite.com', 'https://en.wikipedia.org', 'https://www.wikipedia.org/', 'https://90s-movies.netlify.app/'];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
